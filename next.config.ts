7c3ad02f71e084a71715a70a66fd2bd69529a374
CodeHub007 <smajji3@uic.edu>
2026-02-12 12:24:19 -0600

    Firebase - Error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: { ignoreBuildErrors: true },
+  turbopack: {},
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@firebase/firestore": path.resolve(__dirname, "node_modules/@firebase/firestore"),
+      "@firebase/app": path.resolve(__dirname, "node_modules/@firebase/app"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
